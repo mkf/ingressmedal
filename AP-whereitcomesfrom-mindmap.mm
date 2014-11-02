6b766c6115a1b8357c1ff0e212f89c39a1aca463
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-02 16:13:00 +0100

    Added AP counting

---
+++ AP-whereitcomesfrom-mindmap.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1414936519991" ID="ID_1621071058" MODIFIED="1414938194376" STYLE="bubble" TEXT="AP">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936556200" ID="ID_271507071" MODIFIED="1414938206440" POSITION="right" STYLE="bubble" TEXT="computable">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936575657" ID="ID_1946264471" MODIFIED="1414938206440" STYLE="bubble" TEXT="perfectly">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936623191" ID="ID_604445688" MODIFIED="1414938206440" STYLE="bubble" TEXT="Seer &#x2014; portals discovered">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936637288" ID="ID_1669918425" MODIFIED="1414938206440" STYLE="bubble" TEXT="Builder &#x2014; resonator deployed">
+<edge STYLE="sharp_bezier"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1669918425" ENDARROW="Default" ENDINCLINATION="543;0;" ID="Arrow_ID_638238006" SOURCE="ID_310603851" STARTARROW="None" STARTINCLINATION="543;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936653432" ID="ID_1281121682" MODIFIED="1414938206440" STYLE="bubble" TEXT="Connector &#x2014; links created">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936665816" ID="ID_1666232342" MODIFIED="1414938206440" STYLE="bubble" TEXT="Mind controller &#x2014; Control Fields created">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936695503" ID="ID_1375191998" MODIFIED="1414938206440" STYLE="bubble" TEXT="Liberator &#x2014; portals captured">
+<edge STYLE="sharp_bezier"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1375191998" ENDARROW="Default" ENDINCLINATION="524;0;" ID="Arrow_ID_385137142" SOURCE="ID_310603851" STARTARROW="None" STARTINCLINATION="572;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936707031" ID="ID_975188854" MODIFIED="1414938206440" STYLE="bubble" TEXT="Purifier &#x2014; resonator destroyed">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936949431" ID="ID_1529846198" MODIFIED="1414938206440" STYLE="bubble" TEXT="destroyed links">
+<edge STYLE="sharp_bezier"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1529846198" ENDARROW="Default" ENDINCLINATION="371;0;" ID="Arrow_ID_1494684419" SOURCE="ID_1401361741" STARTARROW="None" STARTINCLINATION="371;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1414936953751" ID="ID_1539804087" MODIFIED="1414938206440" STYLE="bubble" TEXT="destroyed Control Fields">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936964375" ID="ID_1401361741" MODIFIED="1414938206440" STYLE="bubble" TEXT="each 1control fields destroyed = 1 destroyed link">
+<edge STYLE="sharp_bezier"/>
+<arrowlink DESTINATION="ID_1529846198" ENDARROW="Default" ENDINCLINATION="371;0;" ID="Arrow_ID_1494684419" STARTARROW="None" STARTINCLINATION="371;0;"/>
+</node>
+</node>
+</node>
+<node CREATED="1414936580929" ID="ID_1155018395" MODIFIED="1414938206440" STYLE="bubble" TEXT="partly">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936591585" ID="ID_60771874" MODIFIED="1414938206440" STYLE="bubble" TEXT="accurately">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414936750735" ID="ID_96599484" MODIFIED="1414938206440" STYLE="bubble" TEXT="count of recharges">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1414936762319" FOLDED="true" ID="ID_1928124560" MODIFIED="1414938206440" STYLE="bubble" TEXT="estimating that an agent never recharged a portal with XM_max-XM@theMoment&lt;1000">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1414936862175" ID="ID_1308728109" MODIFIED="1414938133783" TEXT="count=~(XMrecharged/1000)+1"/>
+</node>
+<node CREATED="1414936911215" ID="ID_1414374420" MODIFIED="1414938206440" STYLE="bubble" TEXT="we can estimate how many times Agent recharged a portal with less than 1000XM">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+</node>
+<node CREATED="1414936600601" ID="ID_870290101" MODIFIED="1414938343467" STYLE="bubble" TEXT="unaccurately">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414937069815" ID="ID_294547625" MODIFIED="1414938206439" STYLE="bubble" TEXT="hacking enemy portals">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1414937089726" ID="ID_441915375" MODIFIED="1414938206439" STYLE="bubble" TEXT="deploying the last resonator (powering up a portal)">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1414937114717" ID="ID_310603851" MODIFIED="1414938206439" STYLE="bubble" TEXT="maximum is All_deployed&#x2014;portals captured">
+<edge STYLE="sharp_bezier"/>
+<arrowlink DESTINATION="ID_1375191998" ENDARROW="Default" ENDINCLINATION="524;0;" ID="Arrow_ID_385137142" STARTARROW="None" STARTINCLINATION="572;0;"/>
+<arrowlink DESTINATION="ID_1669918425" ENDARROW="Default" ENDINCLINATION="543;0;" ID="Arrow_ID_638238006" STARTARROW="None" STARTINCLINATION="543;0;"/>
+</node>
+</node>
+<node CREATED="1414938362833" ID="ID_1198392816" MODIFIED="1414938392228" TEXT="AP bonuses">
+<node CREATED="1414938476200" ID="ID_1033751373" MODIFIED="1414938479332" TEXT="multiplier"/>
+<node CREATED="1414938479984" ID="ID_1269068355" MODIFIED="1414938499468" TEXT="specified value"/>
+</node>
+</node>
+</node>
+<node CREATED="1414936585096" ID="ID_451092261" MODIFIED="1414938206439" STYLE="bubble" TEXT="may be entered by user">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1414938431832" ID="ID_1139525261" MODIFIED="1414938437348" TEXT="easily">
+<node CREATED="1414938455897" ID="ID_1836249280" MODIFIED="1414938461545" TEXT="portal edits">
+<node CREATED="1414938461546" ID="ID_1500565496" MODIFIED="1414938463828" TEXT="photos"/>
+<node CREATED="1414938464209" ID="ID_1389501306" MODIFIED="1414938465796" TEXT="edits"/>
+</node>
+</node>
+<node CREATED="1414938438105" ID="ID_1093119614" MODIFIED="1414938439996" TEXT="hardly">
+<node CREATED="1414938443073" ID="ID_1827464071" MODIFIED="1414938453932" TEXT="AP from passcodes"/>
+</node>
+</node>
+</node>
+</node>
+</map>
--- ingressmedal.py
+++ ingressmedal.py
@@ -1,91 +1,156 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-class agent:
-	"Klasa dotycząca pojedyńczego agenta"
+class current:
+	"This class applies only to current stats, it doen't compare anything to the past"
 	def __init__(self,codename):
 		print "Input for agent %s" % codename
+		current = {}
 		isint = False
 		while isint == False:
 			try:
-				cap = int(raw_input('Current AP: '))
+				current['ap'] = int(raw_input('Current AP: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cuniqvis = int(raw_input('Current count of unique portals visited: '))
+				current['uniqvis'] = int(raw_input('Current count of unique portals visited: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cseer = int(raw_input('Current count of discovered portals: '))
+				current['seer'] = int(raw_input('Current count of discovered portals: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				chack = int(raw_input('Current count of hacks: '))
+				current['hack'] = int(raw_input('Current count of hacks: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cdepl = int(raw_input('Current count of deployed resonators: '))
+				current['depl'] = int(raw_input('Current count of deployed resonators: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				clink = int(raw_input('Current count of created links: '))
+				current['link'] = int(raw_input('Current count of created links: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cfields = int(raw_input('Current count of created Control Fields: '))
+				current['field'] = int(raw_input('Current count of created Control Fields: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				crech = int(raw_input('Current count of recharged XM: '))
+				current['rech'] = int(raw_input('Current count of recharged XM: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				ccapt = int(raw_input('Current count of captured portals: '))
+				current['capt'] = int(raw_input('Current count of captured portals: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cuniqcapt = int(raw_input('Current count of unique portals captured: '))
+				current['uniqcapt'] = int(raw_input('Current count of unique portals captured: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cdestr = int(raw_input('Current count of destroyed resonators: '))
+				current['destr'] = int(raw_input('Current count of destroyed resonators: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
 		isint = False
 		while isint == False:
 			try:
-				cguard = int(raw_input('Current max time portal held in days: '))
+				current['guard'] = int(raw_input('Current max time portal held in days: '))
 			except ValueError:
 				print "Value is not int"
 				isint = False
+		isint = False
+		while isint == False:
+			try:
+				current['destrlink'] = int(raw_input('Current count of enemy links destroyed: '))
+			except ValueError:
+				print "Value is not int"
+				isint = False
+		isint = False
+		while isint == False:
+			try:
+				current['destrfield'] = int(raw_input('Current count of enemy Control Fields destroyed: '))
+			except ValueError:
+				print "Value is not int"
+				isint = False
+		isintorn = False
+		while isintorn == False:
+			phototry = raw_input("Current count of photos approved to portals (check mail) (number (even if it's 0) or 'n' character if you don't know): ")
+			try:
+				current['photo'] = int(phototry)
+			except ValueError:
+				print "Value is not int"
+				if phototry = "n":
+					current['photo'] = 'n'
+					print "So you don't know, huh?"
+				else:
+					print "It is neither a number nor 'n', you seriously don't know how to answer such a simple question??"
+					isintorn = False
+		while isintorn == False:
+			phototry = raw_input("Current count of photos approved to portals (check mail) (number (even if it's 0) or 'n' character if you don't know): ")
+			try:
+				current['edit'] = int(phototry)
+			except ValueError:
+				print "Value is not int"
+				if phototry = "n":
+					current['edit'] = 'n'
+					print "So you don't know, huh?"
+				else:
+					print "It is neither a number nor 'n', you seriously don't know how to answer such a simple question??"
+					isintorn = False
+		self.current = current
+	def rjeturncount(self):
+		return self.current
+	def coUNTINGcurapcountable(self):
+			current = self.current
+			curapcountable = {
+				'seer' = (current['seer']*1000),
+				'depllater' = ((current['depl']-current['capt'])*125),
+				'link' = (current['link']*313),
+				'field' = (current['field']*1250),
+				'rechmin' = (current['rech']*10),
+				'captres' = (current['capt']*625),
+				'destr' = (current['destr']*75),
+				'destrlink' = (current['destrlink']*187),
+				'destrfield' = (current['destrfield']*750),
+			}
+			if current['photo'] = 'n':
+				print "That's your fault you don't know how much AP you've gained on photos."
+			else:
+				curapcountable['photo'] = (current['photo']*500)
+			if current['edit'] = 'n':
+				print "That's your fault you don't know how much AP you've gained on edits."
+			else:
+				curapcountable['edit'] = (current['edit']*200)
+			return curapcountable
