87887c7c791d4d59ad9c16df3d9f174dbc10c18c
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-19 00:55:46 +0100

    Started working on PastAnalyze, started renaming

---
+++ pastanalyze.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+class pastanalyze:
+	def __init__(self,overs=False):
+		from xmling import xmling
+		from gameinfo import gameinfo
+		gameinf = gameinfo()
+		self.medaldict = gameinf.medaldict
+		self.lvldict = gameinf.lvldict
+		self.highestplaceofnoappearance = gameinf.highestplaceofnoappearance
+		self.namesforcurapcountable = gameinf.namesforcurapcountable
+		self.overs = overs
+
+	#def
