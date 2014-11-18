5e2da4f5f82b5db508dd9feed9e2296fdb4da33f
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-18 23:23:10 +0100

    Changed version to v1.1

--- xmling.py
+++ xmling.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 class xmling:
 	def __init__(self):
-		self.versionhistory = ('0','1.0.1.0')
-		self.currentversion = '1.0.1.0'
+		self.versionhistory = ('0','1.0.1.0','1.1')
+		self.currentversion = '1.1'
 		self.progname = "ingressmedal by ArchieT"
 
 	def opening(self,filename):
