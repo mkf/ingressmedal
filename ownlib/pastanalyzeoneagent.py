16a3994904e2796201303fdf6f6fd55e8386fd49
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-19 01:46:09 +0100

    Working on pastanalyzeoneagent

--- ownlib/pastanalyzeoneagent.py
+++ ownlib/pastanalyzeoneagent.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class pastanalyzeoneagent:
-	def __init__(self,overs=False):
+	def __init__(self,filepath,codename,overs=False):
 		from ownlib.gameinfo import gameinfo
 		gameinf = gameinfo()
 		self.medaldict = gameinf.medaldict
@@ -8,5 +8,16 @@ class pastanalyzeoneagent:
 		self.highestplaceofnoappearance = gameinf.highestplaceofnoappearance
 		self.namesforcurapcountable = gameinf.namesforcurapcountable
 		self.overs = overs
+		from xmling import xmling
+		x = xmling()
+		pbd = x.opening(filepath)
+		pd = x.opendata(pbd['base'])
+		pastija = x.readingentries(pd,codename)
+		pastia = {}
+		for keyik in pastija.keys():
+			pastia[int(keyik)] = pastija[keyik]
+		from clarifydata import clarifydata
+		clar = clarifydata().AskForTheListOfDataToBeSavedFromCurrent
+		tajmy = sorted(pastia.keys())
 
 	#def
