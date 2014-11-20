2246a4dc747f1dced3da5c7c7b603dd7e89f86ec
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-20 17:30:36 +0100

    Moved OcrRead to ownlib dir TODO: steal some ideas from https://github.com/BlueHerons/StatTracker/blob/master/code/OCR.class.php

--- OcrRead.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class OcrRead:
-	def __init__(self,thefile):
-		import tesseract
-		
---
+++ ownlib/OcrRead.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# TODO: steal some ideas from https://github.com/BlueHerons/StatTracker/blob/master/code/OCR.class.php
+
+class OcrRead:
+	def __init__(self,thefile):
+		pass
