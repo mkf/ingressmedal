453da7abd7a5bcc6f9f0a74eddecfef08361c1c7
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-21 03:15:59 +0100

    Added 'please' to the text printing in protection against lack of Seer           ;;;;;;;;;   ocradalterproc works perfectly, but "'capt': 148521," ≠ 1440 – reality

--- ownlib/OcrRead.py
+++ ownlib/OcrRead.py
@@ -46,7 +46,7 @@ class OcrRead:
 					print "My loopinginging is too high thus exiting.",loopinginging
 					quit()
 				if ejo == 'seer':
-					print "Contribute to Ingress and submit at least one portal"
+					print "Contribute to Ingress and submit at least one portal, please"
 					break
 		elementojn = {}
 		for eje in elements.keys():
