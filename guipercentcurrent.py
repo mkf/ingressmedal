a18715de0e7bf9648a709f104e8359f64f2251b6
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-22 16:26:07 +0100

    Working on GUI

--- guipercentcurrent.py
+++ guipercentcurrent.py
@@ -27,6 +27,24 @@ def BigNumberORn(ciag):
 
 
 
+ID_MENU_NEWAGENT = wx.NewId()
+ID_MENU_SWITCHAGENT = wx.NewId()
+ID_MENU_DELENTRY = wx.NewId()
+ID_MENU_SHOWHISTFRAG = wx.NewId()
+ID_MENU_SHAREHISTFRAG = wx.NewId()
+ID_MENU_SHAREENTRY = wx.NewId()
+ID_MENU_SHARECHERRYENTRIES = wx.NewId()
+ID_MENU_SHAREAGENT = wx.NewId()
+ID_MENU_EXPORTBACKUP = wx.NewId()
+ID_MENU_IMPORTSHARED = wx.NewId()
+ID_MENU_IMPORTBACKUP = wx.NewId()
+ID_MENU_NEWDB = wx.NewId()
+ID_MENU_OPENDB = wx.NewId()
+
+ID_OPENNEWWNETRYDIALOG = wx.NewId()
+ID_OPENOCRDIALOG = wx.NewId()  #OCR dialog will contain image preview and text fields for each medal colour for manual color recognition, and when it will become automatic, it will show them
+
+
 from ownlib.interactive import Interactive
 
 interaktywnosciowo = Interactive()
@@ -45,34 +63,13 @@ interaktywnosciowo = Interactive()
 
 class NewEntryDialog(wx.Dialog):
 	def __init__(self,*args,**kw):
-		pass
-
-class MainWindow(wx.Frame):
-	def __init__(self,parent,title):
-		super(MainWindow,self).__init__(parent,title=title,size=(1200,720))
-		menubar = wx.MenuBar()
-		filemenu = wx.Menu()
-		menu = {}
-		menu['NewAgent'] = filemenu.Append(wx.ID_NEW,"New Agent","Create a new Agent")
-		menu['SwitchAgent'] = filemenu.Append(wx.ID_FILECTRL,"Switch Agent","Switch between Agents")
-		menu['DelEntry'] = filemenu.Append(wx.ID_DELETE,"Delete Entry","Choose an entry to delete")
-		menu['ShowHistFrag'] = filemenu.Append(wx.ID_JUMP_TO,"Show a fragment of history","Choose starting and ending entries to show everything between them")
-		menu['ShareHistFrag'] = filemenu.Append(wx.ID_SAVEAS,"Share a fragment of history","Choose starting and ending entries to share everything between them")
-		menu['ShareEntry'] = filemenu.Append(wx.ID_SAVEAS,"Share an entry","Choose an entry to share")
-		menu['ShareEntriesCherry'] = filemenu.Append(wx.ID_SAVEAS,"Share cherry-picked entries","Choose by cherry-picking the entries you want to share")
-		menu['ShareAgent'] = filemenu.Append(wx.ID_SAVEAS,"Share an Agent","Choose the agent to share all the information about")
-		menu['ExportEverything'] = filemenu.Append(wx.ID_SAVEAS,"Export data&config backup","Export the whole database and configuration files")
-		menu['ImportShared'] = filemenu.Append(wx.ID_OPEN,"Import shared file","Choose a file shared to you by someone")
-		menu['ImportEverything'] = filemenu.Append(wx.ID_OPEN,"Import a data&config backup","Choose a data&config backup and import what you want")
-		menu['NewDB'] = filemenu.Append(wx.ID_NEW,"Create a new database","Create a new database file in another path and switch to it")
-		menu['SwitchDB'] = filemenu.Append(wx.ID_OPEN,"Open another database","Switch to another database file")
-		menubar.Append(filemenu, '&File')
-		self.SetMenuBar(menubar)
+		super(NewEntryDialog,self).__init__(*args,**kw)
 		self.InitUI()
-		self.Centre()
-		self.Show()
+		self.SetSize((960,960))
+		self.SetTitle("New Entry")
 
 	def InitUI(self):
+		def InitUI(self):
 		panel = wx.Panel(self,-1)
 
 		font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
@@ -122,6 +119,50 @@ class MainWindow(wx.Frame):
 
 		#midPan = wx.Panel(panel)
 		vs.Add(grid1,1,wx.EXPAND,5)
+
+		panel.SetSizer(vs)
+		vs.Fit(panel)
+		panel.Move((50,50))
+
+		#print a
+
+class OCRDialog(wx.Dialog):
+	def __init__(self,*args,**kw):
+		super(NewEntryDialog,self).__init__(*args,**kw)
+		self.InitUI()
+		self.SetSize((960,960))
+		self.SetTitle("New Entry")
+	def InitUI(self):
+		panel = wx.Panel(self,-1)
+
+		font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
+		font.SetPointSize(8)
+
+		vs = wx.BoxSizer(wx.HORIZONTAL)
+		#box1_title = wx.StaticBox(panel,-1,"Parameters")
+		#box1 = wx.StaticBoxSizer(box1_title,wx.VERTICAL)
+		butgrid = wx.FlexGridSizer(cols=3)
+		vs.Add(butgrid,1,wx.EXPAND,40)
+		panel.SetSizer(vs)
+		vs.Fit(panel)
+		panel.Move((50,50))
+
+class GridDialog(wx.Dialog):
+	def __init__(self,*args,**kw):
+		super(NewEntryDialog,self).__init__(*args,**kw)
+		self.InitUI()
+		self.SetSize((960,960))
+		self.SetTitle("New Entry")
+	def InitUI(self):
+		panel = wx.Panel(self,-1)
+
+		font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
+		font.SetPointSize(8)
+
+		vs = wx.BoxSizer(wx.HORIZONTAL)
+		#box1_title = wx.StaticBox(panel,-1,"Parameters")
+		#box1 = wx.StaticBoxSizer(box1_title,wx.VERTICAL)
+
 		exampel = {'sth':[float(2.86),float(2.56),float(5.23)],'asdf':[float(567.3),float(43.5),float(78.4)]}
 
 		mleng = max([len([test for test in exampel[i]]) for i in exampel.keys()])+1
@@ -144,6 +185,50 @@ class MainWindow(wx.Frame):
 
 
 		vs.Add(grid2,1,wx.EXPAND|wx.ALIGN_RIGHT,40)
+
+		panel.SetSizer(vs)
+		vs.Fit(panel)
+		panel.Move((50,50))
+
+		#print a
+
+class MainWindow(wx.Frame):
+	def __init__(self,parent,title):
+		super(MainWindow,self).__init__(parent,title=title,size=(1200,720))
+		menubar = wx.MenuBar()
+		filemenu = wx.Menu()
+		menu = {}
+		menu['NewAgent'] = filemenu.Append(ID_MENU_NEWAGENT,"New Agent","Create a new Agent")
+		menu['SwitchAgent'] = filemenu.Append(ID_MENU_SWITCHAGENT,"Switch Agent","Switch between Agents")
+		menu['DelEntry'] = filemenu.Append(ID_MENU_DELENTRY,"Delete Entry","Choose an entry to delete")
+		menu['ShowHistFrag'] = filemenu.Append(ID_MENU_SHOWHISTFRAG,"Show a fragment of history","Choose starting and ending entries to show everything between them")
+		menu['ShareHistFrag'] = filemenu.Append(ID_MENU_SHAREHISTFRAG,"Share a fragment of history","Choose starting and ending entries to share everything between them")
+		menu['ShareEntry'] = filemenu.Append(ID_MENU_SHAREENTRY,"Share an entry","Choose an entry to share")
+		menu['ShareEntriesCherry'] = filemenu.Append(ID_MENU_SHARECHERRYENTRIES,"Share cherry-picked entries","Choose by cherry-picking the entries you want to share")
+		menu['ShareAgent'] = filemenu.Append(ID_MENU_SHAREAGENT,"Share an Agent","Choose the agent to share all the information about")
+		menu['ExportEverything'] = filemenu.Append(ID_MENU_EXPORTBACKUP,"Export data&config backup","Export the whole database and configuration files")
+		menu['ImportShared'] = filemenu.Append(ID_MENU_IMPORTSHARED,"Import shared file","Choose a file shared to you by someone")
+		menu['ImportEverything'] = filemenu.Append(ID_MENU_IMPORTBACKUP,"Import a data&config backup","Choose a data&config backup and import what you want")
+		menu['NewDB'] = filemenu.Append(ID_MENU_NEWDB,"Create a new database","Create a new database file in another path and switch to it")
+		menu['SwitchDB'] = filemenu.Append(ID_MENU_OPENDB,"Open another database","Switch to another database file")
+		menubar.Append(filemenu, '&File')
+		self.SetMenuBar(menubar)
+		self.InitUI()
+		self.Centre()
+		self.Show()
+
+	def InitUI(self):
+		panel = wx.Panel(self,-1)
+
+		font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
+		font.SetPointSize(8)
+
+		vs = wx.BoxSizer(wx.HORIZONTAL)
+		#box1_title = wx.StaticBox(panel,-1,"Parameters")
+		#box1 = wx.StaticBoxSizer(box1_title,wx.VERTICAL)
+
+		butgrid = wx.FlexGridSizer(cols=3)
+		vs.Add(butgrid,1,wx.EXPAND,40)
 
 		panel.SetSizer(vs)
 		vs.Fit(panel)
