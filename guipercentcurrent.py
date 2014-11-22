1ff65e7c7efe60844694d2d2dddfc58c799930d3
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-22 15:26:27 +0100

    Working on GUI

--- guipercentcurrent.py
+++ guipercentcurrent.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-import argparse
 import wx
-from Current import Current
+from ownlib.Current import Current
 
 def TrueOrFalse(ciag):
 	if (ciag == "True") or (ciag == "y") or (ciag == "yes"):
@@ -10,7 +9,7 @@ def TrueOrFalse(ciag):
 	elif (ciag == "False") or (ciag == "n") or (ciag == "no"):
 		return False
 	else:
-		raise argparse.ArgumentTypeError('It is not True nor False, y nor n, yes nor no. Although it had to.')
+		raise ValueError('It is not True nor False, y nor n, yes nor no. Although it had to.')
 
 
 def BigNumberORn(ciag):
@@ -23,55 +22,48 @@ def BigNumberORn(ciag):
 			elif range(0, 99999).index(int(ciag)) == 0:
 				return int(ciag)
 		except:
-			raise argparse.ArgumentTypeError('It is not "n" nor a number. Although it had to.')
+			raise ValueError('It is not "n" nor a number. Although it had to.')
 
 
-argh = argparse.ArgumentParser()
 
-from interactive import Interactive
-
+from ownlib.interactive import Interactive
 
 interaktywnosciowo = Interactive()
-argumentydodane = []
-for keyowo in interaktywnosciowo.GivMeCurQSdict().keys():
-	argh.add_argument(
-		('-' + keyowo),
-		('--' + keyowo),
-		type=int,
-		help=(interaktywnosciowo.GivMeCurQSdict()[keyowo]))
-	argumentydodane.append(keyowo)
-for keyowko in interaktywnosciowo.GivMeCurQUSdict().keys():
-	argh.add_argument(
-		('-' + keyowko),
-		('--' + keyowko),
-		type=BigNumberORn,
-		help=(interaktywnosciowo.GivMeCurQUSdict()[keyowko]))
-	argumentydodane.append(keyowo)
-argh.add_argument('-i', '--interactively', type=TrueOrFalse, help="Interactively (True/False)")
-argh.add_argument('-o', '--overs', type=TrueOrFalse, help="Show overs (True/False)")
-parmetry = vars(argh.parse_args())
+#argumentydodane = []
+#for keyowo in interaktywnosciowo.GivMeCurQSdict().keys():
+#	argh.add_argument([...]
+#		type=int,
+#		help=(interaktywnosciowo.GivMeCurQSdict()[keyowo]))
+#	argumentydodane.append(keyowo)
+#for keyowko in interaktywnosciowo.GivMeCurQUSdict().keys():
+#	argh.add_argument([...]
+#		type=BigNumberORn,
+#		help=(interaktywnosciowo.GivMeCurQUSdict()[keyowko]))
+#	argumentydodane.append(keyowo)
+#argh.add_argument('-o', '--overs', type=TrueOrFalse, help="Show overs (True/False)")
 
 class MainWindow(wx.Frame):
 	def __init__(self,parent,title):
 		wx.Frame.__init__(self,parent,title=title,size=(1200,720))
-		self.CreateStatusBar()
+		menubar = wx.MenuBar()
 		filemenu = wx.Menu()
-		menuNewAgent = filemenu.Append(wx.ID_NEW,"New Agent","Create a new Agent")
-		menuSwitchAgent = filemenu.Append(wx.ID_FILECTRL,"Switch Agent","Switch between Agents")
-		menuNewEntry = filemenu.Append(wx.ID_NEW,"New Entry","Create a new stats entry")
-		menuDelEntry = filemenu.Append(wx.ID_DELETE,"Delete Entry","Choose an entry to delete")
-		menuShowHistFrag = filemenu.Append(wx.ID_JUMP_TO,"Show a fragment of history","Choose starting and ending entries to show everything between them")
-		menuShareHistFrag = filemenu.Append(wx.ID_SAVEAS,"Share a fragment of history","Choose starting and ending entries to share everything between them")
-		menuShareEntry = filemenu.Append(wx.ID_SAVEAS,"Share an entry","Choose an entry to share")
-		menuShareEntriesCherry = filemenu.Append(wx.ID_SAVEAS,"Share chery-picked entries","Choose by cherry-picking the entries you want to share")
-		menuShareAgent = filemenu.Append(wx.ID_SAVEAS,"Share an Agent","Choose the agent to share all the information about")
-		menuExportEverything = filemenu.Append(wx.ID_SAVEAS,"Export data&config backup","Export the whole database and configuration files")
-		menuImportShared = filemenu.Append(wx.ID_OPEN,"Import shared file","Choose a file shared to you by someone")
-		menuImportEverything = filemenu.Append(wx.ID_OPEN,"Import a data&config backup","Choose a data&config backup and import what you want")
+		manu = {}
+		menu['NewAgent'] = filemenu.Append(wx.ID_NEW,"New Agent","Create a new Agent")
+		menu['SwitchAgent'] = filemenu.Append(wx.ID_FILECTRL,"Switch Agent","Switch between Agents")
+		menu['DelEntry'] = filemenu.Append(wx.ID_DELETE,"Delete Entry","Choose an entry to delete")
+		menu['ShowHistFrag'] = filemenu.Append(wx.ID_JUMP_TO,"Show a fragment of history","Choose starting and ending entries to show everything between them")
+		menu['ShareHistFrag'] = filemenu.Append(wx.ID_SAVEAS,"Share a fragment of history","Choose starting and ending entries to share everything between them")
+		menu['ShareEntry'] = filemenu.Append(wx.ID_SAVEAS,"Share an entry","Choose an entry to share")
+		menu['ShareEntriesCherry'] = filemenu.Append(wx.ID_SAVEAS,"Share chery-picked entries","Choose by cherry-picking the entries you want to share")
+		menu['ShareAgent'] = filemenu.Append(wx.ID_SAVEAS,"Share an Agent","Choose the agent to share all the information about")
+		menu['ExportEverything'] = filemenu.Append(wx.ID_SAVEAS,"Export data&config backup","Export the whole database and configuration files")
+		menu['ImportShared'] = filemenu.Append(wx.ID_OPEN,"Import shared file","Choose a file shared to you by someone")
+		menu['ImportEverything'] = filemenu.Append(wx.ID_OPEN,"Import a data&config backup","Choose a data&config backup and import what you want")
+		menu['NewDB'] = filemenu.Append(wx.ID_NEW,"Create a new database","Create a new database file in another path and switch to it")
+		menu['SwitchDB'] = filemenu.Append(wx.ID_OPEN,"Open another database","Switch to another database file")
 
-class ParamPanel(wx.Panel):
-	def __init__(self,parent):
-		wx.Panel.__init__(self,parent,-1)
+	def InitUI(self):
+		wx.Panel.__init__(self,-1)
 		panel = wx.Panel(self,-1)
 		vs = wx.BoxSizer(wx.HORIZONTAL)
 		box1_title = wx.StaticBox(panel,-1,"Parameters")
