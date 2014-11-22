9fc875d946ac017a431b25cb4e26494227bd6c61
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-11-22 16:26:07 +0100

    Working on moving to Django

--- webserver/ingressmedal/settings.py
+++ webserver/ingressmedal/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'ingressmedal.ingressmedal'
+#	'ingressmedal.ocrentry'
 )
 
 MIDDLEWARE_CLASSES = (
