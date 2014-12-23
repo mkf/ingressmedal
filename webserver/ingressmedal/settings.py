0b6b163f1694fd8d2c56b94f22532f6b7d0ad512
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-12-23 22:54:54 +0100

    Working on moving to Django: should there be "ingressmedal.statsanalyze" or just "statsanalyze" in "INSTALLED_APPS"?

--- webserver/ingressmedal/settings.py
+++ webserver/ingressmedal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'ingressmedal.statsanalyze'
+	'statsanalyze'
 )
 
 MIDDLEWARE_CLASSES = (
