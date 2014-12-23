60729ab29cf26807e5ec7fe70e6a52304af3e5a8
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-12-23 23:42:25 +0100

    Working on moving to Django: models

--- webserver/statsanalyze/models.py
+++ webserver/statsanalyze/models.py
@@ -1,9 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Entry(models.Model):
-	entrdate = models.DateTimeField('Entry date')
+	agentdb = models.ForeignKey('Agent')
+	creator = models.ForeignKey('auth.User')
+	currentowner = models.ForeignKey('auth.User')
+	public = models.BooleanField()
+	ocreddirectly = models.BooleanField()
+	idnumber = models.CharField(max_length=50) # unix time + 10 random characters
+	codename = models.CharField(max_length=50)
+	entry_date = models.DateTimeField('Entry datetime')
+	added_time = models.DateTimeField(default=timezone.now)
+	parama = models.PositiveIntegerField()
+	paramb = models.PositiveIntegerField()
+	paramc = models.PositiveIntegerField()
+
+	def addentry(self):
+		self.added_time = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return str(self.codename)+" "+str(self.idnumber)
 
 #class Stat(models.Model):
 #
