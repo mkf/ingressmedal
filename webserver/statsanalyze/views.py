2dab4adf992938c74649757f8738fe7ecb1dc3d1
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-09 04:04:52 +0100

    Working on moving to Django

--- webserver/statsanalyze/views.py
+++ webserver/statsanalyze/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def agents_list(request):
-	return render(request,'statsanalyze/agents_list.html',{})
+	agents = Agent.objects.filter(public=True).order_by('codename')
+	return render(request,'statsanalyze/agents_list.html',{'agents': agents})
