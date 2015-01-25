a4233eb59bc8dae45f7c0ed81af5d936978964e6
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-25 08:59:40 +0100

    Working on moving to Django

--- webserver/ingressmedal/settings.py
+++ webserver/ingressmedal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'social.appps.django_app.default',
+	'social.apps.django_app.default',
 	'statsanalyze'
 )
 
