4f57d7d9c0fc68f06daceefbbcc1db0de00d3e46
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-26 15:50:39 +0100

    Working on moving to Django: unwanted parentheses

--- webserver/statsanalyze/urls.py
+++ webserver/statsanalyze/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = patterns(
 	'',
 	url(r'^$',views.glowna),
-	url(r'^$',views.agents_list()),
+	url(r'^$',views.agents_list),
 	url(r'^login.html$',views.login),
 	url(r'^login_error.html$',views.login_error),
 #	url('', include('social.apps.django_app.urls', namespace='social')),
