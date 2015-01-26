5623a307ba51ffb1d5b44101495e48a539279e51
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-26 16:09:55 +0100

    Working on moving to Django: agents_list listing, temporarily removed filtering by 'public'. There will be a search for public agentsdbs, and there will be 'watched' and 'owned'.

--- webserver/statsanalyze/views.py
+++ webserver/statsanalyze/views.py
@@ -6,7 +6,7 @@ from .models import *
 def glowna(request):
 	return render(request,'glowna.html',{})
 def agents_list(request):
-	agents = Agent.objects.filter(public=True).order_by('codename')
+	agents = Agent.objects.filter().order_by('codename') # TODO: public and private
 	return render(request,'agents_list.html',{'agents': agents})
 def login(request):
 	return render(request,'login.html',{})
