161e455d55d415a0c8498e4177d29de917e5de45
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-26 14:05:34 +0100

    Working on moving to Django: No 'guardnow' anymore, only alltime&month&week guardian measurements

--- webserver/statsanalyze/admin.py
+++ webserver/statsanalyze/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(AllTimeEntry)
-admin.site.register(MonthEntry)
-admin.site.register(WeekEntry)
+admin.site.register(Entry)
 admin.site.register(Agent)
 admin.site.register(User)
--- webserver/statsanalyze/models.py
+++ webserver/statsanalyze/models.py
@@ -3,7 +3,16 @@ from django.utils import timezone
 
 # Create your models here.
 
-class AllTimeEntry(models.Model):
+class Entry(models.Model):
+	alltime = 'AllTime'
+	month = 'Month'
+	week = 'Week'
+	timepartchoices = (
+		(alltime,'AllTime'),
+		(month,'Month'),
+		(week,'Week'),
+	)
+	timepart = models.CharField(max_length=7,choices=timepartchoices,default=alltime)
 	agentdb = models.ForeignKey('Agent')
 	creator = models.ForeignKey('User',related_name='entrycreator')
 	owner = models.ForeignKey('User',related_name='entryowner')
@@ -51,105 +60,6 @@ class AllTimeEntry(models.Model):
 
 	def __str__(self):
 		return str(self.agentdb.codename)+" "+str(self.idnumber)
-
-class MonthEntry(models.Model):
-	agentdb = models.ForeignKey('Agent')
-	creator = models.ForeignKey('User',related_name='entrycreator')
-	owner = models.ForeignKey('User',related_name='entryowner')
-	public = models.BooleanField()
-	ocreddirectly = models.BooleanField()
-	idnumber = models.CharField(max_length=50) # unix time + 10 random characters
-	entry_date = models.DateTimeField('Entry datetime')
-	added_time = models.DateTimeField(default=timezone.now())
-	ap = models.PositiveIntegerField()
-	bronze = models.PositiveIntegerField()
-	silver = models.PositiveIntegerField()
-	gold = models.PositiveIntegerField()
-	platinum = models.PositiveIntegerField()
-	onyx = models.PositiveIntegerField()
-	nomedal = models.PositiveIntegerField()
-	uniqvis = models.PositiveIntegerField()
-	seer = models.PositiveIntegerField()
-	xm = models.PositiveIntegerField()
-	walk = models.PositiveIntegerField()
-	depl = models.PositiveIntegerField()
-	link = models.PositiveIntegerField()
-	field = models.PositiveIntegerField()
-	allfieldmusum = models.PositiveIntegerField()
-	longestlink = models.PositiveIntegerField()
-	largestfield = models.PositiveIntegerField()
-	rech = models.PositiveIntegerField()
-	capt = models.PositiveIntegerField()
-	uniqcapt = models.PositiveIntegerField()
-	mods = models.PositiveIntegerField()
-	destr = models.PositiveIntegerField()
-	neutr = models.PositiveIntegerField()
-	destrlink = models.PositiveIntegerField()
-	destrfield = models.PositiveIntegerField()
-	guard = models.PositiveIntegerField()
-#	guardnow = models.PositiveIntegerField(blank=True)
-	guardlink = models.PositiveIntegerField()
-	maxlinklenxdays = models.PositiveIntegerField()
-	guardfield = models.PositiveIntegerField()
-	maxfieldmuxdays = models.PositiveIntegerField()
-	hack = models.PositiveIntegerField()
-	edits = models.PositiveIntegerField(blank=True)
-	photos = models.PositiveIntegerField(blank=True)
-	mods = models.PositiveIntegerField()
-	recruiter = models.PositiveIntegerField(blank=True)
-
-	def __str__(self):
-		return str(self.agentdb.codename)+" "+str(self.idnumber)
-
-class WeekEntry(models.Model):
-	agentdb = models.ForeignKey('Agent')
-	creator = models.ForeignKey('User',related_name='entrycreator')
-	owner = models.ForeignKey('User',related_name='entryowner')
-	public = models.BooleanField()
-	ocreddirectly = models.BooleanField()
-	idnumber = models.CharField(max_length=50) # unix time + 10 random characters
-	entry_date = models.DateTimeField('Entry datetime')
-	added_time = models.DateTimeField(default=timezone.now())
-	ap = models.PositiveIntegerField()
-	bronze = models.PositiveIntegerField()
-	silver = models.PositiveIntegerField()
-	gold = models.PositiveIntegerField()
-	platinum = models.PositiveIntegerField()
-	onyx = models.PositiveIntegerField()
-	nomedal = models.PositiveIntegerField()
-	uniqvis = models.PositiveIntegerField()
-	seer = models.PositiveIntegerField()
-	xm = models.PositiveIntegerField()
-	walk = models.PositiveIntegerField()
-	depl = models.PositiveIntegerField()
-	link = models.PositiveIntegerField()
-	field = models.PositiveIntegerField()
-	allfieldmusum = models.PositiveIntegerField()
-	longestlink = models.PositiveIntegerField()
-	largestfield = models.PositiveIntegerField()
-	rech = models.PositiveIntegerField()
-	capt = models.PositiveIntegerField()
-	uniqcapt = models.PositiveIntegerField()
-	mods = models.PositiveIntegerField()
-	destr = models.PositiveIntegerField()
-	neutr = models.PositiveIntegerField()
-	destrlink = models.PositiveIntegerField()
-	destrfield = models.PositiveIntegerField()
-	guard = models.PositiveIntegerField()
-#	guardnow = models.PositiveIntegerField(blank=True)
-	guardlink = models.PositiveIntegerField()
-	maxlinklenxdays = models.PositiveIntegerField()
-	guardfield = models.PositiveIntegerField()
-	maxfieldmuxdays = models.PositiveIntegerField()
-	hack = models.PositiveIntegerField()
-	edits = models.PositiveIntegerField(blank=True)
-	photos = models.PositiveIntegerField(blank=True)
-	mods = models.PositiveIntegerField()
-	recruiter = models.PositiveIntegerField(blank=True)
-
-	def __str__(self):
-		return str(self.agentdb.codename)+" "+str(self.idnumber)
-
 
 class Agent(models.Model):
 	codename = models.CharField(max_length=50)
