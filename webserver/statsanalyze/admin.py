a9288bb618616529ca45469813e9b50464684fa0
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-01-26 13:54:36 +0100

    Working on moving to Django: AllTimeEntry, MonthEntry, WeekEntry

--- webserver/statsanalyze/admin.py
+++ webserver/statsanalyze/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Entry)
+admin.site.register(AllTimeEntry)
+admin.site.register(MonthEntry)
+admin.site.register(WeekEntry)
 admin.site.register(Agent)
 admin.site.register(User)
